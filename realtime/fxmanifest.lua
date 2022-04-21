de4ca6112493200335644a533afa3518028956e0
Nicolas Noël <epicman94@gmail.com>
2022-04-21 14:45:04 +0200

    Add files via upload

---
+++ realtime/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+
+game "gta5"
+author "StevenAfroman"
+description "changer l'heurs du jeu en heurs réelle"
+version "1.0.0"
+
+client_script "client.lua"
+server_script "server.lua"
